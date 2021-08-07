17f11864fd58e076523fd121b7670cd71c0f5bfe
Samuel Shear <dspfgh34@gmail.com>
2021-08-07 02:33:26 -0400

    Finished app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BoardComponent } from './components/board/board.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { SquareComponent } from './components/square/square.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    SquareComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/board/board.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss'],
+})
+export class BoardComponent implements OnInit {
+  squares!: string[];
+  winner!: string | null;
+  xNext!: boolean;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.squares = Array(9).fill(null);
+    this.winner = null;
+    this.xNext = true;
+  }
+
+  calculateWinner(): string | null{
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        this.squares[a] &&
+        this.squares[a] === this.squares[b] &&
+        this.squares[a] === this.squares[c]
+      ) {
+        return this.squares[a];
+      }
+    }
+    return null;
+  }
+
+  get player(): string {
+    return this.xNext ? 'X' : 'O';
+  }
+
+  makeMove(id: number) {
+    if (!this.squares[id]) {
+      this.squares.splice(id, 1, this.player);
+      this.xNext = !this.xNext;
+    }
+
+    this.winner = this.calculateWinner();
+  }
+}
